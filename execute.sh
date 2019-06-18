d99b3837dcd0b1c7a1c76c6003852a1115fe74d4
dkozak <dkozak94@gmail.com>
2019-06-19 00:01:40 +0100

    prepare basic structure for the source code

--- execute.sh
+++ execute.sh
@@ -2,10 +2,14 @@
 SPARK_BIN="/home/dkozak/spark-2.4.3-bin-hadoop2.7/bin"
 
 JAR_FILE="build/libs/spark-assignment-1.0-SNAPSHOT.jar"
+INPUT_FILE="./employee_reviews.csv"
+OUTPUT_FILE="./results.out"
+
+rm -f ${OUTPUT_FILE}
 
 #if [[ ! -f ${JAR_FILE} ]]; then
 #    gradle jar || exit 1
 #fi
 
-gradle jar && ${SPARK_BIN}/spark-submit --class io.dkozak.estg.spark.assignment.MainKt --master "local[4]" ${JAR_FILE}
+gradle jar && ${SPARK_BIN}/spark-submit --class io.dkozak.estg.spark.assignment.MainKt --master "local[4]" ${JAR_FILE} ${INPUT_FILE} ${OUTPUT_FILE}
 
--- src/main/kotlin/io/dkozak/estg/spark/assignment/main.kt
+++ src/main/kotlin/io/dkozak/estg/spark/assignment/main.kt
@@ -1,17 +1,43 @@
 package io.dkozak.estg.spark.assignment
 
 import org.apache.spark.sql.SparkSession
+import java.io.BufferedWriter
+import java.io.File
 
-fun main() {
-    val inputFile = "/home/dkozak/projects/spark-assignment/employee_reviews.csv"
-    val spark = SparkSession.builder().appName("test")
+
+fun handleArguments(args: Array<String>): Pair<String, String> {
+    args.size != 2 && throw IllegalArgumentException("Two arguments expected, input csv file and output file")
+    val inputFile = args[0]
+    val outputFile = args[1]
+    return inputFile to outputFile
+}
+
+fun sparkExecute(block: (SparkSession) -> Unit) {
+    val spark = SparkSession.builder().appName("App")
         .orCreate
-    val dataset = spark.read().textFile(inputFile).cache()
+    try {
+        block(spark)
+    } finally {
+        spark.stop()
+    }
 
-    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ${dataset.first()}")
-    val count = dataset.filter { it.length > 3 }
-        .count()
+}
 
-    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! $count")
-    spark.stop()
+fun BufferedWriter.println(text: String) = this.write("$text\n")
+
+fun fileOutput(path: String, block: (BufferedWriter) -> Unit) = File(path).bufferedWriter().use(block)
+
+fun main(args: Array<String>) {
+    val (inputFile, outputFile) = handleArguments(args)
+    sparkExecute { spark ->
+        fileOutput(outputFile) { output ->
+            val dataset = spark.read().textFile(inputFile).cache()
+            output.println("First line: ${dataset.first()}")
+            val count = dataset.filter { it.length > 3 }
+                .count()
+
+            output.println("Line count: $count")
+        }
+    }
+
 }
