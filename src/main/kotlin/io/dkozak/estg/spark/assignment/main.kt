4eaae1128b5fa365f5c4acf859e208c8cea4108a
dkozak <dkozak94@gmail.com>
2019-06-19 10:35:20 +0100

    prepare the structure of the project for easy to use task addition

--- src/main/kotlin/io/dkozak/estg/spark/assignment/main.kt
+++ src/main/kotlin/io/dkozak/estg/spark/assignment/main.kt
@@ -1,15 +1,35 @@
 package io.dkozak.estg.spark.assignment
 
+import io.dkozak.estg.spark.assignment.tasks.lookupCollection
+import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.SparkSession
 import java.io.BufferedWriter
 import java.io.File
 
 
-fun handleArguments(args: Array<String>): Pair<String, String> {
-    args.size != 2 && throw IllegalArgumentException("Two arguments expected, input csv file and output file")
-    val inputFile = args[0]
-    val outputFile = args[1]
-    return inputFile to outputFile
+const val LOG_FILE_NAME = "log"
+
+val allTasks = listOf(lookupCollection)
+
+
+fun handleArguments(args: Array<String>): Triple<String, String, List<AssignmentTask>> {
+    fun fail(message: String): Nothing = throw IllegalArgumentException(message)
+
+    args.size < 2 || args.size > 3 && fail("Args: input_csv_file output_directory [task_number]")
+    val inputFile = File(args[0])
+    inputFile.exists() || fail("File ${args[0]} does not exist")
+    val outputDir = File(args[1])
+    if (outputDir.exists()) {
+        outputDir.isDirectory || fail("${args[1]} exists and is not a directory")
+    } else {
+        outputDir.mkdir() || fail("Could not create the output directory")
+    }
+    return if (args.size == 2) Triple(args[0], args[1], allTasks)
+    else {
+        val task = args[2].toIntOrNull() ?: fail("${args[2]} is not an integer")
+        (task < 1 || task > 12) && fail("$task should be between 1 and 12")
+        Triple(args[0], args[1], allTasks.subList(task - 1, task))
+    }
 }
 
 fun sparkExecute(block: (SparkSession) -> Unit) {
@@ -20,24 +40,24 @@ fun sparkExecute(block: (SparkSession) -> Unit) {
     } finally {
         spark.stop()
     }
-
 }
 
 fun BufferedWriter.println(text: String) = this.write("$text\n")
 
-fun fileOutput(path: String, block: (BufferedWriter) -> Unit) = File(path).bufferedWriter().use(block)
+fun prepareOutput(outputDir: String, block: (BufferedWriter) -> Unit) =
+    File("$outputDir/$LOG_FILE_NAME").bufferedWriter().use(block)
+
+typealias AssignmentTask = (dataset: Dataset<String>, outputDir: String, log: (String) -> Unit) -> Unit
 
 fun main(args: Array<String>) {
-    val (inputFile, outputFile) = handleArguments(args)
+    val (inputFile, outputDir, tasks) = handleArguments(args)
     sparkExecute { spark ->
-        fileOutput(outputFile) { output ->
+        prepareOutput(outputDir) { logger ->
             val dataset = spark.read().textFile(inputFile).cache()
-            output.println("First line: ${dataset.first()}")
-            val count = dataset.filter { it.length > 3 }
-                .count()
-
-            output.println("Line count: $count")
+            for (task in tasks) {
+                task(dataset, outputDir, logger::println)
+            }
         }
-    }
 
+    }
 }
---
+++ src/main/kotlin/io/dkozak/estg/spark/assignment/tasks/1.kt
@@ -0,0 +1,7 @@
+package io.dkozak.estg.spark.assignment.tasks
+
+import io.dkozak.estg.spark.assignment.AssignmentTask
+
+val lookupCollection: AssignmentTask = { dataset, outputDir, log ->
+
+}
